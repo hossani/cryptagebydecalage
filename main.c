ea28663f104a47e5f65f749b439220678912c84f
Idder Ait El Mouden <139082259+IDDER29@users.noreply.github.com>
2024-03-12 13:29:26 +0000

    add a loop

--- main.c
+++ main.c
@@ -34,13 +34,28 @@ char* dec_cry(char *chaine)
 
 int main()
 {
-    char user_str_input[100];
 
-    printf("Enter your Text that you want to crypt : ");
-    scanf("%s", user_str_input);
-    dec_cry(user_str_input);
-    printf("%s", user_str_input);
-
+    do
+    {
+        char user_str_input[100];
+        char question;
+        char *pt;
+        printf("\nClick on c for encryption otherwise on d for decryption c/d:\t");
+        scanf(" %c",&question);
+        printf("\nEnter your Text that you want to crypt : ");
+        scanf("%s", user_str_input);
+        if(question=='c')
+        {
+            pt=cry(user_str_input);
+            printf("%s\n", pt);
+        }
+        if(question == 'd')
+        {
+            pt=dec_cry(user_str_input);
+            printf("%s\n", pt);
+        }
+    }
+    while(1);
 
     return 0;
 }
