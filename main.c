3cb7a57a92f3c7f4eb6ed443a52ca78f643c62c8
Idder Ait El Mouden <139082259+IDDER29@users.noreply.github.com>
2024-03-12 11:44:04 +0000

    add the cry and dec_cry

--- main.c
+++ main.c
@@ -1,21 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-char get_The_letter(char *str){
 
-
-
-    for(int i = 0; i< strlen(str); i++){
-        if(str[i]== '\0'){
-            break;
+char* cry(char *chaine)
+{
+    char* ptr = chaine;
+    while(*ptr!='\0')
+    {
+        if(*ptr!=' ')
+        {
+            *ptr=*ptr+3;
         }
-
-        printf("letter : %c\n", str[i]);
-
-        //crypt_str(str[i]);
+        ptr++;
     }
-
+    return chaine ;
 }
+
+char* dec_cry(char *chaine)
+{
+    char* ptr = chaine;
+    while(*ptr!='\0')
+    {
+        if(*ptr!=' ')
+        {
+            *ptr=*ptr-3;
+        }
+        ptr++;
+    }
+    return chaine ;
+}
+
+
 
 int main()
 {
@@ -23,7 +38,8 @@ int main()
 
     printf("Enter your Text that you want to crypt : ");
     scanf("%s", user_str_input);
-    get_The_letter(user_str_input);
+    dec_cry(user_str_input);
+    printf("%s", user_str_input);
 
 
     return 0;
