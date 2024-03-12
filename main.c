4496703d34ff84e51317ef8d857d9fd49c5cd8da
hossani <hamza.hossani.001@gmail.com>
2024-03-12 12:28:04 +0000

    ss

--- main.c
+++ main.c
@@ -1,28 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-<<<<<<< HEAD
-char* cry(char *chaine);
-int main()
-{
-	char chaine[]="hamza hossani";
-	char *ptr=cry(chaine);
-    printf("%s\n",ptr);
 
-    return 0;
-}
-
-char* cry(char *chaine){
-char* ptr=chaine;
-while(*ptr!='\0'){
-		if(*ptr!=' '){
-				*ptr=*ptr+3;
-		}
-	ptr++;
-}
-return chaine;
-}
-=======
 
 char* cry(char *chaine)
 {
@@ -66,4 +45,3 @@ int main()
 
     return 0;
 }
->>>>>>> 381041a0b0ee907c2d676c4bb039e84f45296239
